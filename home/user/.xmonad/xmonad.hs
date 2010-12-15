32536c18c877a0946c5b4b873561bdfad2e18d42
Mathias Svensson <freaken@freaken.dk>
2010-12-15 23:34:42 +0100

    Added another workspace, added support for colemak layout, added hotkeys for changing to the left/right workspace

--- home/user/.xmonad/xmonad.hs
+++ home/user/.xmonad/xmonad.hs
@@ -12,6 +12,8 @@ import XMonad.Layout.Accordion
 import XMonad.Layout.NoBorders
 import XMonad.Layout
 import System.IO
+import Data.List (dropWhile, drop, takeWhile, find)
+import Data.Maybe()
 
 statusBarCmd= "dzen2 -bg '#1a1a1a' -fg '#ffffff' -h 12 -w 480 -sa c -e '' -ta l -fn -*-*-*-*-*-*-12-*-*-*-*-*-iso10646-1" 
 dmenuCmd= "dmenu_run -nb '#1a1a1a' -nf '#ffffff' -sb '#aecf96' -sf black -p '>'"
@@ -21,9 +23,9 @@ main = do din <- spawnPipe statusBarCmd
           xmonad $ defaultConfig
     
 	               { manageHook         = myManageHook <+> manageDocks <+> manageHook defaultConfig
-                   , layoutHook         = smartBorders (myLayout)
+                   , layoutHook         = smartBorders myLayout
                    , keys               = myKeys
-	               , workspaces         = map show [0 .. 9 :: Int] ++ ["a", "b", "c", "d"]
+	               , workspaces         = map show [0 .. 9 :: Int] ++ map (: []) ['a' .. 'e']
                    , logHook            = dynamicLogWithPP $ myPP din
                    , modMask            = mod4Mask     -- Rebind Mod to the Windows key
                    , normalBorderColor  = "#555555"
@@ -35,12 +37,12 @@ main = do din <- spawnPipe statusBarCmd
 -- ManageHooks
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ className =? "Pidgin" --> doShift "d"
-    , className =? "Skype" --> doShift "d"
-    , className =? "Transmission" --> doShift "c"
-    , className =? "Deluge" --> doShift "c"
-    , className =? "Quodlibet" --> doShift "b"
-    , className =? "Wine" --> doFloat
+    [ className =? "Pidgin" --> doShift "e"
+    , className =? "Skype" --> doShift "e"
+    , className =? "Transmission" --> doShift "d"
+    , className =? "Deluge" --> doShift "d"
+    , className =? "Quodlibet" --> doShift "c"
+--    , className =? "Wine" --> doFloat
     , isFullscreen --> doFullFloat]
 
 
@@ -61,7 +63,7 @@ myPP h = defaultPP
 
 -- Keys
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+myKeys conf@(XConfig {XMonad.modMask = modMask, XMonad.workspaces = workspaces}) = M.fromList $
     -- killing programs
     [ ((modMask .|. shiftMask, xK_c ), kill)
 
@@ -101,8 +103,18 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-[1..9] %! Switch to workspace N
     -- mod-shift-[1..9] %! Move client to workspace N
     [((m .|. modMask, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) ([xK_onehalf] ++ [xK_1 .. xK_9] ++ [xK_0, xK_plus, 65105, xK_BackSpace]) -- 65105 is the key after the plus key on my keyboard
+        | (i, k) <- zip workspaces (xK_onehalf : [xK_1 .. xK_9] ++ [xK_0, xK_plus, 65105, xK_BackSpace, xK_Home]) ++ -- 65105 is the key after the plus key on my keyboard
+                    zip ["0", "b", "c"] [xK_grave, xK_minus, xK_equal] -- Colemak layout
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    -- mod-[a,s] + mod-[left,right]:
+    --      move to left/right workspace
+    -- mod-shift-[a,s] + mod-shift-[left,right]:
+    --      move to left/right workspace that contains anything
+    [((m .|. modMask, k), windows $ f workspaces i)
+        | (i, k) <- [(-1, xK_a), (1, xK_s), (-1, xK_Left), (1, xK_Right)]
+        , (m, f) <- [(0, getTag False), (shiftMask, getTag True)]
+    ]
     ++
     -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
@@ -110,3 +122,18 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
+getTag :: Bool -> [String] -> Int -> WindowSet -> WindowSet
+getTag allowEmpty all_tags diff s = if diff == 0 then s else
+    W.greedyView new_tag s
+    where cur_tag = W.currentTag s
+          possible_next = if diff > 0
+                          then drop 1  $ dropWhile (/=cur_tag) all_tags
+                          else reverse $ takeWhile (/=cur_tag) all_tags
+          getStack tag = find ((==tag) . W.tag) (W.workspaces s) >>= W.stack
+          filter_empty = maybe False (not . null . W.integrate) . getStack
+          filtered = if allowEmpty
+                     then possible_next
+                     else filter filter_empty possible_next
+          new_tag = if null filtered
+                    then cur_tag
+                    else filtered !! ((min (abs diff) (length filtered))-1)
